8c233f660ada8a8be8f2ad34b929383392928c89
ishmael <ishmael@blackbalto.com>
2011-04-19 14:02:16 +0100

    change generators to haml

--- Gemfile
+++ Gemfile
@@ -6,7 +6,8 @@ gem 'rails', '3.0.4'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
-
+gem 'haml'
+gem 'haml-rails'
 # Use unicorn as the web server
 # gem 'unicorn'
 
--- config/application.rb
+++ config/application.rb
@@ -31,12 +31,16 @@ module SenchaTouchStore
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w()
+    #config.action_view.javascript_expansions[:defaults] = %w()
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    config.generators do |g|
+      g.template_engine :haml
+    end
   end
 end
