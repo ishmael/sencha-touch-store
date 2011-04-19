edce723a62d1559ffa9193947720e4d1a917568f
ishmael <ishmael@blackbalto.com>
2011-04-19 14:31:19 +0100

    fix error in migration

--- db/migrate/20110419132521_create_products.rb
+++ db/migrate/20110419132521_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration
   def self.up
     create_table :products do |t|
-      t.string : title
+      t.string :title
       t.text :description
       t.string :image_url
       t.decimal :price, :precision => 8, :scale => 2
