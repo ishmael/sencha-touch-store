c0203c48c1dbd1ac160549bdd459817e92f207d9
ishmael <ishmael@blackbalto.com>
2011-04-19 14:25:11 +0100

    adjust for the mobile browsers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :adjust_format_for_iphone
+
+  private
+
+    # Set iPhone format if request to iphone.trawlr.com
+    def adjust_format_for_iphone
+      request.format = :iphone if mobile_device?
+    end  
+
+    def mobile_device?
+      user_agent = request.user_agent
+      user_agent =~ /Mobile|webOS/
+    end
+    helper_method :mobile_device?
 end
