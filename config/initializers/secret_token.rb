1740e7840334f9b275c903537aac604d1e692422
ishmael <ishmael@blackbalto.com>
2011-04-19 14:01:01 +0100

    created app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SenchaTouchStore::Application.config.secret_token = '9264e1e3bb70c11032cd76fbc0374a9fab0f96a35cff2b907035bbd3c6dd992229bbbdd7310bce8a526e7c456e87f136105a6467cff29b33ae96edbaed8bdf54'
