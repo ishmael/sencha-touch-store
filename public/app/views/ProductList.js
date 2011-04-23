0885306ae3d4d99f435ab0fda7b988651f54eff8
ishmael <ishmael@blackbalto.com>
2011-04-23 19:06:19 +0100

    add tab to the bottom

--- public/app/views/ProductList.js
+++ public/app/views/ProductList.js
@@ -1,45 +1,41 @@
-app.views.ProductList = new Ext.Panel ( {
-        items: [{
-
-            xtype: 'list',
-            store:  app.stores.products,
-            itemTpl: '<div class="contact"><strong>{title}</strong></div>',
-            grouped: true,
-            indexBar: true,
-            plugins: [{
-                ptype: 'pullrefresh'
-            }],
-
-        }],
+app.views.ProductList = new Ext.TabPanel ( {
         layout: 'fit',
         listeners:{
             activate : function(){
                 app.stores.products.load();
             }
-        },dockedItems: [{
-        xtype: 'toolbar',
-        ui: 'light',
-        dock: 'bottom',
-        defaults: {
-            iconMask: true,
-            ui: 'plain'
-        },
-        scroll: {
-            direction: 'horizontal',
-            useIndicators: false
-        },
-        layout: {
-            pack: 'center'
         },
         items: [
-            {
-            iconCls: 'list'
+                {
+                    title: 'Lista',
+                    iconCls: 'list',
+                        layout: 'fit',                    
+                    items: [{
+                        xtype: 'list',
+                        store:  app.stores.products,
+                        itemTpl: '<div class="contact"><strong>{title}</strong></div>',
+                        grouped: true,
+                        indexBar: true,
+                        plugins: [{
+                            ptype: 'pullrefresh'
+                        }]
+                    }]
+                },
+                {
+                    title: 'adicionar',
+                    iconCls: 'add',
+                    handler: function() {
+                        console.log('lets add');
+                    }
+        }],
+        tabBar: {
+            dock: 'bottom',
+            scroll: {
+            direction: 'horizontal',
+            useIndicators: false
             },
-            {
-                iconCls: 'add',
-                handler: function() {
-                    console.log('lets add');
-                }
-            }]
-        }]
+            layout: {
+                pack: 'center'
+            }
+        }
 });
