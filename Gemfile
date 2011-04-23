7c86f989fc96ab2f6cf988cf8544d4f0d2af2046
ishmael <ishmael@blackbalto.com>
2011-04-23 18:54:48 +0100

    add compass

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'sqlite3'
 
 gem 'haml'
 gem 'haml-rails'
-
+gem 'compass',
 gem "jquery-rails"
 
 # Use unicorn as the web server
--- public/app/views/ProductList.js
+++ public/app/views/ProductList.js
@@ -16,5 +16,30 @@ app.views.ProductList = new Ext.Panel ( {
             activate : function(){
                 app.stores.products.load();
             }
-        }
+        },dockedItems: [{
+        xtype: 'toolbar',
+        ui: 'light',
+        dock: 'bottom',
+        defaults: {
+            iconMask: true,
+            ui: 'plain'
+        },
+        scroll: {
+            direction: 'horizontal',
+            useIndicators: false
+        },
+        layout: {
+            pack: 'center'
+        },
+        items: [
+            {
+            iconCls: 'list'
+            },
+            {
+                iconCls: 'add',
+                handler: function() {
+                    console.log('lets add');
+                }
+            }]
+        }]
 });
